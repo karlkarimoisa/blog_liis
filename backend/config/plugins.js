97b557d9213de89e028324fec96c898e2a98abd8
karlkarimoisa <karl.karimoisa@tmd.ee>
2020-07-09 15:46:45 +0300

    add cloudinary2

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET')
+        cloud_name: env('dvmhmims5'),
+      api_key: env('219543247919919'),
+      api_secret: env('S1orKCMZIgabYStsE7kQYArU4Yg')
         
       },
     }
