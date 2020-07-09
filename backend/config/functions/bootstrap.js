349124bd7af757c65a9b8e4dab15e46739f3a90e
karlkarimoisa <karl.karimoisa@tmd.ee>
2020-07-09 17:23:20 +0300

    plseddasdasd jahg

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -12,14 +12,6 @@
 
 
 
-module.exports = ({env}) => ({
-    upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: 'dvmhmims5',
-            api_key: '219543247919919',
-            api_secret: 'S1orKCMZIgabYStsE7kQYArU4Yg'
-          },
-        
-      }
+module.exports = () => ({
+    
 });
