c3a2d0d6f15fa6c1d6e772afe5d2d9aaabd8b541
karlkarimoisa <karl.karimoisa@tmd.ee>
2020-07-09 15:12:38 +0300

    parandus

--- front/src/containers/Articles/index.js
+++ front/src/containers/Articles/index.js
@@ -8,7 +8,8 @@ const Home = () => {
     <div>
       <div className="uk-section">
         <div className="uk-container uk-container-large">
-          <h1>Liisbetii blogi</h1>
+          <h1>Liisbeti blogi</h1>
+          <h2>Kirjutan siia mida tahan</h2>
           <Query query={ARTICLES_QUERY}>
             {({ data: { articles } }) => {
               return <Articles articles={articles} />;
